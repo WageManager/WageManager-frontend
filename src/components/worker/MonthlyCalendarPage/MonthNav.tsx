69b69c0f5bb24b6271ea3fe88c24e2cf4a52d28f
l2juhan <leejuhan0527@gmail.com>
2026-01-21 15:28:39 +0900

    feat:MonthNav.tsx 화살표 react-icon으로 변경

--- src/components/worker/MonthlyCalendarPage/MonthNav.tsx
+++ src/components/worker/MonthlyCalendarPage/MonthNav.tsx
@@ -1,9 +1,6 @@
-/**
- * MonthNav - 월 네비게이션 컴포넌트
- * 이전/다음 월로 이동하는 네비게이션 UI
- */
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
-/** Props 타입 (이 컴포넌트에서만 사용) */
 interface MonthNavProps {
   year: number;
   month: number; // 1-12 (표시용)
@@ -20,13 +17,13 @@ export default function MonthNav({
   return (
     <div className="month-nav">
       <button className="month-nav-arrow" onClick={onPrevMonth} type="button">
-        {'<'}
+        <IoIosArrowBack/>
       </button>
       <div className="month-nav-title">
         {year}년 {month}월
       </div>
       <button className="month-nav-arrow" onClick={onNextMonth} type="button">
-        {'>'}
+        <IoIosArrowForward />
       </button>
     </div>
   );
