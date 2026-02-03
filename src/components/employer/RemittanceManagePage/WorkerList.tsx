c35a2e17f107470a1c284b920c4ab4528090e746
l2juhan <leejuhan0527@gmail.com>
2026-02-03 16:23:49 +0900

    feat:employer remittancePage 컴포넌트 생성

---
+++ src/components/employer/RemittanceManagePage/WorkerList.tsx
@@ -0,0 +1,27 @@
+/**
+ * 근로자 목록 컴포넌트 (왼쪽 패널)
+ */
+
+import type { WorkerListProps } from "../../../types/employer/employerRemittancePage.types";
+
+export default function WorkerList({
+  workers,
+  selectedWorkerId,
+  onWorkerClick,
+}: WorkerListProps) {
+  return (
+    <div className="remittance-worker-list">
+      {workers.map((worker) => (
+        <div
+          key={worker.id}
+          className={`worker-item ${
+            selectedWorkerId === worker.id ? "selected" : ""
+          }`}
+          onClick={() => onWorkerClick(worker)}
+        >
+          {worker.workerName}
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/employer/RemittanceManagePage/WorkplaceSelect.tsx
@@ -0,0 +1,28 @@
+/**
+ * 근무지 선택 드롭다운 컴포넌트
+ */
+
+import type { WorkplaceSelectProps } from "../../../types/employer/employerRemittancePage.types";
+
+export default function WorkplaceSelect({
+  workplaces,
+  selectedWorkplaceId,
+  onChange,
+}: WorkplaceSelectProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newWorkplaceId = Number(e.target.value);
+    onChange(newWorkplaceId);
+  };
+
+  return (
+    <div className="remittance-workplace-select">
+      <select value={selectedWorkplaceId ?? ""} onChange={handleChange}>
+        {workplaces.map((wp) => (
+          <option key={wp.id} value={wp.id}>
+            {wp.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
