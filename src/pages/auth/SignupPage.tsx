c8980a9f8fd1ca910503ab8756bc8ee64edbc9c5
wonyeoe <yeowonlee04@gmail.com>
2026-02-07 01:22:08 +0900

    SignupPage 직접 접근 가능하도록 -> 다 하고 되돌려 놓을 것!!

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -8,9 +8,9 @@ export default function SignupPage() {
 
   // 사용자가 카카오 액세스 토큰 없이 직접 접근하는 것(/signup) 방지 
   // -> 화면 테스트할땐 지우시면 됩니다!
-  if (!kakaoAccessToken) {
-    return <Navigate to="/" replace />;
-  }
+  // if (!kakaoAccessToken) {
+  //   return <Navigate to="/" replace />;
+  // }
 
   return (
     <div className="signup-container">
